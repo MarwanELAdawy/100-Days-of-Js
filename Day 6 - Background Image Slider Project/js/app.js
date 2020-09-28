065444aefe388dd4895fa82e4458c09dac7cfe89
Marwan El-Adawy <MarwanELAdawy@users.noreply.github.com>
2020-09-28 12:26:40 +0200

    Day 6

---
+++ Day 6 - Background Image Slider Project/js/app.js
@@ -0,0 +1,30 @@
+const pictures = [
+  "contBcg-0",
+  "contBcg-1",
+  "contBcg-2",
+  "contBcg-3",
+  "contBcg-4"
+];
+  //select both left and right button. Add event listeners
+const buttons = document.querySelectorAll('.btn')
+const imageDIV = document.querySelector('.img-container')
+let counter = 0
+buttons.forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if (button.classList.contains('btn-left')){
+      counter--;
+      if(counter < 0){
+        counter = pictures.length -1;
+      }
+      imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`;
+    }
+    if (button.classList.contains('btn-right')){
+      counter++;
+      if(counter > pictures.length -1){
+        counter = 0;
+      }
+      imageDIV.style.backgroundImage = `url('./img/${pictures[counter]}.jpeg')`;
+    }
+  })
+})
+
